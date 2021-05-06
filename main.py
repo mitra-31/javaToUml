a07f44b4ab57ad76e243c767ba8b3984885f962d
mitra-31 <69145473+mitra-31@users.noreply.github.com>
2021-05-06 12:00:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-#@Mitra
-#@pavani
+#@Mitra-31
+#@pavani-234
 
-variable = int(input())
+
+
+sourceCode = ""  # Appending the source code to the sourceCode variable.
+
+
+print("Enter/Paste your code.Ctrl-Z ( windows ) to save it.")    # when we hit Ctrl+z it will invoke except and it stops taking input from user. 
+while True:
+  try:
+    sourceCode += input(">> ") + "\t"
+  except EOFERROR:
+    break
+    
+    
+#print(sourceCode)
